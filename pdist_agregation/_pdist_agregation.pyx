5c1e9ffd50f9f004f6ba71a38c7afe0d788b1f44
Julien Jerphanion <git@jjerphan.xyz>
2021-05-05 10:41:51 +0200

    Refactor code
    Create a private cython module with the implementations
    for both chunking strategies.
    
    Use openmp to get the maximum number of threads.
    
    .gitignore taken from:
    https://github.com/zouzias/cython-examples/blob/master/.gitignore

---
+++ pdist_agregation/_pdist_agregation.pyx
@@ -0,0 +1,381 @@
+# cython: language_level=3
+# cython: profile=True
+# cython: cdivision=True
+# cython: boundscheck=False
+# cython: wraparound=False
+import numpy as np
+cimport numpy as np
+cimport openmp
+
+from libc.stdlib cimport malloc, free
+from libc.string cimport memset
+from libc.stdio cimport printf
+from cython.parallel cimport prange, parallel
+from cython cimport floating, integral
+
+# TODO: Best size found by a quick tuning, can be improved
+DEF CHUNK_SIZE = 4096
+DEF INF = 1e19
+
+from sklearn.utils._cython_blas cimport _gemm, BLAS_Order, BLAS_Trans
+from sklearn.utils._cython_blas cimport ColMajor, RowMajor, Trans, NoTrans
+
+### Heaps utilities
+
+cdef int _push(
+    floating* dist,
+    integral* idx,
+    integral size,
+    floating val,
+    integral i_val,
+) nogil except -1:
+    """push (val, i_val) into the heap (dist, idx) of the given size"""
+    cdef:
+        integral i, ic1, ic2, i_swap
+
+    # check if val should be in heap
+    if val > dist[0]:
+        return 0
+
+    # insert val at position zero
+    dist[0] = val
+    idx[0] = i_val
+
+    # descend the heap, swapping values until the max heap criterion is met
+    i = 0
+    while True:
+        ic1 = 2 * i + 1
+        ic2 = ic1 + 1
+
+        if ic1 >= size:
+            break
+        elif ic2 >= size:
+            if dist[ic1] > val:
+                i_swap = ic1
+            else:
+                break
+        elif dist[ic1] >= dist[ic2]:
+            if val < dist[ic1]:
+                i_swap = ic1
+            else:
+                break
+        else:
+            if val < dist[ic2]:
+                i_swap = ic2
+            else:
+                break
+
+        dist[i] = dist[i_swap]
+        idx[i] = idx[i_swap]
+
+        i = i_swap
+
+    dist[i] = val
+    idx[i] = i_val
+
+    return 0
+
+
+cdef inline void dual_swap(
+    floating* dist,
+    integral* idx,
+    integral i1,
+    integral i2
+) nogil:
+    """swap the values at inex i1 and i2 of both dist and idx"""
+    cdef:
+        floating dtmp = dist[i1]
+        integral itmp = idx[i1]
+
+    dist[i1] = dist[i2]
+    dist[i2] = dtmp
+
+    idx[i1] = idx[i2]
+    idx[i2] = itmp
+
+
+cdef int _simultaneous_sort(
+    floating* dist,
+    integral* idx,
+    integral size
+) nogil except -1:
+    """
+    Perform a recursive quicksort on the dist array, simultaneously
+    performing the same swaps on the idx array.
+    """
+    cdef:
+        integral pivot_idx, i, store_idx
+        floating pivot_val
+
+    # in the small-array case, do things efficiently
+    if size <= 1:
+        pass
+    elif size == 2:
+        if dist[0] > dist[1]:
+            dual_swap(dist, idx, 0, 1)
+    elif size == 3:
+        if dist[0] > dist[1]:
+            dual_swap(dist, idx, 0, 1)
+        if dist[1] > dist[2]:
+            dual_swap(dist, idx, 1, 2)
+            if dist[0] > dist[1]:
+                dual_swap(dist, idx, 0, 1)
+    else:
+        # Determine the pivot using the median-of-three rule.
+        # The smallest of the three is moved to the beginning of the array,
+        # the middle (the pivot value) is moved to the end, and the largest
+        # is moved to the pivot index.
+        pivot_idx = size / 2
+        if dist[0] > dist[size - 1]:
+            dual_swap(dist, idx, 0, size - 1)
+        if dist[size - 1] > dist[pivot_idx]:
+            dual_swap(dist, idx, size - 1, pivot_idx)
+            if dist[0] > dist[size - 1]:
+                dual_swap(dist, idx, 0, size - 1)
+        pivot_val = dist[size - 1]
+
+        # partition indices about pivot.  At the end of this operation,
+        # pivot_idx will contain the pivot value, everything to the left
+        # will be smaller, and everything to the right will be larger.
+        store_idx = 0
+        for i in range(size - 1):
+            if dist[i] < pivot_val:
+                dual_swap(dist, idx, i, store_idx)
+                store_idx += 1
+        dual_swap(dist, idx, store_idx, size - 1)
+        pivot_idx = store_idx
+
+        # recursively sort each side of the pivot
+        if pivot_idx > 1:
+            _simultaneous_sort(dist, idx, pivot_idx)
+        if pivot_idx + 2 < size:
+            _simultaneous_sort(dist + pivot_idx + 1,
+                               idx + pivot_idx + 1,
+                               size - pivot_idx - 1)
+    return 0
+
+### K-NN helpers
+
+cdef void _k_closest_on_chunk(
+    floating[:, ::1] X_c,            # IN
+    floating[:, ::1] Y_c,            # IN
+    floating[::1] Y_sq_norms,        # IN
+    floating *dist_middle_terms,     # IN
+    floating *heap_red_distances,    # IN/OUT
+    integral *heap_indices,          # IN/OUT
+    integral k,                      # IN
+    # ID of the first element of Y_c
+    integral Y_idx_offset,
+) nogil:
+    cdef:
+        integral i, j
+    # Instead of computing the full pairwise squared distances matrix,
+    # ||X_c - Y_c||² = ||X_c||² - 2 X_c.Y_c^T + ||Y_c||², we only need
+    # to store the - 2 X_c.Y_c^T + ||Y_c||² term since the argmin
+    # for a given sample X_c^{i} does not depend on ||X_c^{i}||²
+
+    # Careful: LDA, LDB and LDC are given for F-ordered arrays.
+    # Here, we use their counterpart values as indicated in the documentation.
+    # See the documentation of parameters here:
+    # https://www.netlib.org/lapack/explore-html/db/dc9/group__single__blas__level3_gafe51bacb54592ff5de056acabd83c260.html
+    #
+    # dist_middle_terms = -2 * X_c.dot(Y_c.T)
+    _gemm(RowMajor, NoTrans, Trans,
+          X_c.shape[0], Y_c.shape[0], X_c.shape[1],
+          -2.0,
+          &X_c[0, 0], X_c.shape[1],
+          &Y_c[0, 0], X_c.shape[1], 0.0,
+          dist_middle_terms, Y_c.shape[0])
+
+    # Computing argmins here
+    for i in range(X_c.shape[0]):
+        for j in range(Y_c.shape[0]):
+            _push(heap_red_distances + i * k,
+                  heap_indices + i * k,
+                  k,
+                  # reduced distance: - 2 X_c_i.Y_c_j^T + ||Y_c_j||²
+                  dist_middle_terms[i * Y_c.shape[0] + j] + Y_sq_norms[j],
+                  j + Y_idx_offset)
+
+
+cdef int _parallel_knn_single_chunking(
+    floating[:, ::1] X,              # IN
+    floating[:, ::1] Y,              # IN
+    floating[::1] Y_sq_norms,        # IN
+    integral chunk_size,
+    integral[:, ::1] knn_indices,    # OUT
+) nogil except -1:
+    cdef:
+        integral n_samples_chunk = chunk_size / X.shape[1]
+
+        integral X_n_samples_chunk = min(X.shape[0], n_samples_chunk)
+        integral X_n_full_chunks = X.shape[0] // X_n_samples_chunk
+        integral X_n_samples_rem = X.shape[0] % X_n_samples_chunk
+
+        # Counting remainder chunk in total number of chunks
+        integral X_n_chunks = X_n_full_chunks + (X.shape[0] != (X_n_full_chunks * X_n_samples_chunk))
+
+        integral n_chunks = X_n_chunks
+        integral num_threads = min(n_chunks, openmp.omp_get_max_threads())
+
+        integral idx = 0
+        integral j = 0
+        integral i_chunk = 0
+        integral k = knn_indices.shape[1]
+
+        integral X_start, X_end
+        integral X_chunk_idx
+        floating *dist_middle_terms
+
+    with nogil, parallel(num_threads=num_threads):
+        # Thread local buffers
+
+        # Temporary buffer for the -2 * X_c.dot(Y_c.T) term
+        dist_middle_terms = <floating*> malloc(Y.shape[0] * X_n_samples_chunk * sizeof(floating))
+        heap_red_distances = <floating*> malloc(X_n_samples_chunk * k * sizeof(floating))
+
+        for X_chunk_idx in prange(X_n_chunks, schedule='static'):
+            # We reset the heap between X chunks
+            memset(heap_red_distances, <integral> INF, X_n_samples_chunk * k * sizeof(floating))
+
+            X_start = X_chunk_idx * X_n_samples_chunk
+            if X_chunk_idx == X_n_chunks - 1 and X_n_samples_rem > 0:
+                X_end = X_start + X_n_samples_rem
+            else:
+                X_end = X_start + X_n_samples_chunk
+
+            _k_closest_on_chunk(
+                X[X_start:X_end, :],
+                Y,
+                Y_sq_norms,
+                dist_middle_terms,
+                heap_red_distances,
+                &knn_indices[X_start, 0],
+                k,
+                0,
+            )
+
+            # Registering the indices of the k-nn in order
+            for idx in range(X_end - X_start):
+                _simultaneous_sort(
+                    heap_red_distances + idx * k,
+                    &knn_indices[X_start + idx, 0],
+                    k
+                )
+
+        # end: for X_chunk_idx
+        free(dist_middle_terms)
+        free(heap_red_distances)
+
+    # end: with nogil, parallel
+
+
+cdef int _parallel_knn_double_chunking(
+    floating[:, ::1] X,              # IN
+    floating[:, ::1] Y,              # IN
+    floating[::1] Y_sq_norms,        # IN
+    integral chunk_size,
+    integral[:, ::1] knn_indices,    # OUT
+) nogil except -1:
+    cdef:
+        integral n_samples_chunk = chunk_size / X.shape[1]
+
+        integral X_n_samples_chunk = min(X.shape[0], n_samples_chunk)
+        integral X_n_full_chunks = X.shape[0] // X_n_samples_chunk
+        integral X_n_samples_rem = X.shape[0] % X_n_samples_chunk
+
+        integral Y_n_samples_chunk = min(Y.shape[0], n_samples_chunk)
+        integral Y_n_full_chunks = Y.shape[0] / Y_n_samples_chunk
+        integral Y_n_samples_rem = Y.shape[0] % Y_n_samples_chunk
+
+        # Counting remainder chunk in total number of chunks
+        integral X_n_chunks = X_n_full_chunks + (X.shape[0] != (X_n_full_chunks * X_n_samples_chunk))
+        integral Y_n_chunks = Y_n_full_chunks + (Y.shape[0] != (Y_n_full_chunks * Y_n_samples_chunk))
+
+        integral n_chunks = X_n_chunks * Y_n_chunks
+        integral num_threads = min(n_chunks, openmp.omp_get_max_threads())
+
+        integral idx = 0
+        integral j = 0
+        integral i_chunk = 0
+        integral k = knn_indices.shape[1]
+
+        integral X_start, X_end, Y_start, Y_end
+        integral X_chunk_idx, Y_chunk_idx
+
+        floating *dist_middle_terms
+
+    with nogil, parallel(num_threads=num_threads):
+        # Thread local buffers
+
+        # Temporary buffer for the -2 * X_c.dot(Y_c.T) term
+        dist_middle_terms = <floating*> malloc(Y_n_samples_chunk * X_n_samples_chunk * sizeof(floating))
+        heap_red_distances = <floating*> malloc(X_n_samples_chunk * k * sizeof(floating))
+
+        for X_chunk_idx in prange(X_n_chunks, schedule='static'):
+            # We reset the heap between X chunks
+            memset(heap_red_distances, <integral> INF, X_n_samples_chunk * k * sizeof(floating))
+
+            X_start = X_chunk_idx * X_n_samples_chunk
+            if X_chunk_idx == X_n_chunks - 1 and X_n_samples_rem > 0:
+                X_end = X_start + X_n_samples_rem
+            else:
+                X_end = X_start + X_n_samples_chunk
+
+            for Y_chunk_idx in range(Y_n_chunks):
+                Y_start = Y_chunk_idx * Y_n_samples_chunk
+                if Y_chunk_idx == Y_n_chunks - 1 and Y_n_samples_rem > 0:
+                    Y_end = Y_start + Y_n_samples_rem
+                else:
+                    Y_end = Y_start + Y_n_samples_chunk
+
+                _k_closest_on_chunk(
+                    X[X_start:X_end, :],
+                    Y[Y_start:Y_end, :],
+                    Y_sq_norms[Y_start:Y_end],
+                    dist_middle_terms,
+                    heap_red_distances,
+                    &knn_indices[X_start, 0],
+                    k,
+                    Y_start
+                )
+            # end: for Y_chunk_idx
+
+            # Getting the indices of the k-closest points in
+            # the sorted order
+            for idx in range(X_end - X_start):
+                _simultaneous_sort(
+                    heap_red_distances + idx * k,
+                    &knn_indices[X_start + idx, 0],
+                    k
+                )
+
+        # end: for X_chunk_idx
+        free(dist_middle_terms)
+        free(heap_red_distances)
+
+    # end: with nogil, parallel
+
+
+# Python interface
+
+def parallel_knn(
+    floating[:, ::1] X,
+    floating[:, ::1] Y,
+    integral k,
+    integral chunk_size = CHUNK_SIZE,
+    bint use_chunks_on_Y = True,
+):
+    int_dtype = np.int32 if integral is int else np.int64
+    float_dtype = np.float32 if floating is float else np.float64
+    cdef:
+        integral[:, ::1] knn_indices = np.full((X.shape[0], k), 0,
+                                               dtype=int_dtype)
+        floating[::1] Y_sq_norms = np.einsum('ij,ij->i', Y, Y)
+
+    if use_chunks_on_Y:
+        _parallel_knn_double_chunking(X, Y, Y_sq_norms, chunk_size, knn_indices)
+    else:
+        _parallel_knn_single_chunking(X, Y, Y_sq_norms, chunk_size, knn_indices)
+
+    return np.asarray(knn_indices)
---
+++ tests/test_correctness.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pytest
+from pdist_agregation import parallel_knn
+from sklearn.neighbors import NearestNeighbors
+
+
+@pytest.mark.parametrize("n", [10 ** i for i in [2, 3, 4]])
+@pytest.mark.parametrize("d", [2, 5, 10, 100])
+@pytest.mark.parametrize("n_neighbors", [1, 10, 100])
+@pytest.mark.parametrize("use_chunks_on_Y", [True, False])
+def test_correctness(n, d, n_neighbors, use_chunks_on_Y):
+    np.random.seed(1)
+    Y = np.random.rand(int(n * d)).reshape((-1, d))
+    X = np.random.rand(int(n * d // 2)).reshape((-1, d))
+
+    neigh = NearestNeighbors(n_neighbors=n_neighbors, algorithm='brute')
+    neigh.fit(Y)
+
+    knn_sk = neigh.kneighbors(X, return_distance=False)
+    knn = parallel_knn(X, Y, k=n_neighbors, use_chunks_on_Y=use_chunks_on_Y)
+
+    np.testing.assert_array_equal(knn, knn_sk)
