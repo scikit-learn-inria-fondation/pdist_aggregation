3b5442d559e366117dadafabd868bd0629c069e5
Julien Jerphanion <git@jjerphan.xyz>
2021-05-10 11:23:07 +0200

    Add input validation on the new implementation
    So that it mimics sklearn's behavior.

--- src/nn.py
+++ src/nn.py
@@ -1,5 +1,5 @@
 from pdist_agregation import parallel_knn
-
+from sklearn.utils.validation import check_array
 
 class NearestNeighbors:
     def __init__(self, n_neighbors=1, algorithm="brute"):
@@ -18,6 +18,7 @@ class NearestNeighbors:
     def kneighbors(self, X,
                    working_memory=4_000_000,
                    return_distance=False):
+        X = check_array(X, order="C")
         return parallel_knn(X, self.X_,
                             k=self.n_neighbors,
                             working_memory=working_memory,
