afdc48295c98c8241fd788f85c427f9dfdaf2307
Julien Jerphanion <git@jjerphan.xyz>
2021-05-12 10:49:55 +0200

    Parametrise tests on the ratio of numbers of vectors for both sets

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -6,15 +6,16 @@ from sklearn.neighbors import NearestNeighbors
 
 @pytest.mark.parametrize("n", [10 ** i for i in [2, 3, 4]])
 @pytest.mark.parametrize("d", [2, 5, 10, 100])
+@pytest.mark.parametrize("ratio_train_test", [10, 2, 1, 0.5])
 @pytest.mark.parametrize("n_neighbors", [1, 10, 100])
 @pytest.mark.parametrize("use_chunks_on_Y", [True, False])
 @pytest.mark.parametrize("working_memory", [2 ** i for i in range(10, 20)])
-#@pytest.mark.parametrize("dtype", [np.float32, np.float64])
-def test_correctness(n, d, n_neighbors, use_chunks_on_Y,
+def test_correctness(n, d, ratio_train_test,
+                     n_neighbors, use_chunks_on_Y,
                      working_memory, dtype=np.float64):
     np.random.seed(1)
     Y = np.random.rand(int(n * d)).astype(dtype).reshape((-1, d))
-    X = np.random.rand(int(n * d // 2)).astype(dtype).reshape((-1, d))
+    X = np.random.rand(int(n * d // ratio_train_test)).astype(dtype).reshape((-1, d))
 
     neigh = NearestNeighbors(n_neighbors=n_neighbors, algorithm='brute')
     neigh.fit(Y)
