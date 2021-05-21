531ef73c855138c88d2bc02c4a8640bcec6460e5
Julien Jerphanion <git@jjerphan.xyz>
2021-05-21 09:34:55 +0200

    Modify profiling script for blocking example

--- tests/profiling.py
+++ tests/profiling.py
@@ -4,15 +4,18 @@ from pdist_aggregation import parallel_knn
 
 
 def main(args=None):
-    n = 1e4
-    d = 100
+    d = 50
     n_neighbors = 100
-    chunk_size = 1024
+    chunk_size = 256
     np.random.seed(1)
-    X_train = np.random.rand(int(n * d)).reshape((-1, d))
-    X_test = np.random.rand(int(n * d // 2)).reshape((-1, d))
-
-    parallel_knn(X_train, X_test, k=n_neighbors, chunk_size=chunk_size)
+    X_train = np.random.rand(int(1000 * d)).reshape((-1, d))
+    X_test = np.random.rand(int(1000000 * d // 2)).reshape((-1, d))
+    parallel_knn(
+        X_train, X_test,
+        k=n_neighbors,
+        chunk_size=chunk_size,
+        use_chunk_on_train=True,
+    )
 
 
 if __name__ == "__main__":
