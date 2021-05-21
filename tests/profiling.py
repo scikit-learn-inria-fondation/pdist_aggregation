9c75361fe888326ff816891e09d2ad1bcba2619d
Julien Jerphanion <git@jjerphan.xyz>
2021-05-21 10:03:40 +0200

    Revert "Modify profiling script for blocking example"
    This reverts commit 531ef73c855138c88d2bc02c4a8640bcec6460e5.
    
    Running the implementation sequentially is just slow
    when not using parallelisation.

--- tests/profiling.py
+++ tests/profiling.py
@@ -4,18 +4,15 @@ from pdist_aggregation import parallel_knn
 
 
 def main(args=None):
-    d = 50
+    n = 1e4
+    d = 100
     n_neighbors = 100
-    chunk_size = 256
+    chunk_size = 1024
     np.random.seed(1)
-    X_train = np.random.rand(int(1000 * d)).reshape((-1, d))
-    X_test = np.random.rand(int(1000000 * d // 2)).reshape((-1, d))
-    parallel_knn(
-        X_train, X_test,
-        k=n_neighbors,
-        chunk_size=chunk_size,
-        use_chunk_on_train=True,
-    )
+    X_train = np.random.rand(int(n * d)).reshape((-1, d))
+    X_test = np.random.rand(int(n * d // 2)).reshape((-1, d))
+
+    parallel_knn(X_train, X_test, k=n_neighbors, chunk_size=chunk_size)
 
 
 if __name__ == "__main__":
