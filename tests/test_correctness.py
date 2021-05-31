e6158c066c55b3d3b606d0bf550fd274f7f949f7
Julien Jerphanion <git@jjerphan.xyz>
2021-05-31 16:15:52 +0200

    Test results for float32 data against float64

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -18,8 +18,8 @@ def test_correctness(
     ratio_train_test,
     n_neighbors,
     chunk_size,
+    dtype,
     strategy,
-    dtype=np.float64,
 ):
     if n_samples < n_neighbors:
         pytest.skip(
@@ -52,3 +52,41 @@ def test_correctness(
     )
 
     np.testing.assert_array_equal(knn, knn_sk)
+
+
+@pytest.mark.parametrize("n_samples", [10 ** i for i in [2, 3, 4]])
+@pytest.mark.parametrize("n_features", [2, 5, 10, 100])
+@pytest.mark.parametrize("ratio_train_test", [10, 2, 1, 0.5])
+@pytest.mark.parametrize("n_neighbors", [1, 10, 100, 1000])
+@pytest.mark.parametrize("chunk_size", [2 ** i for i in range(9, 13)])
+@pytest.mark.parametrize("strategy", ["chunk_on_train", "chunk_on_test"])
+def test_float32_against_float64(
+    n_samples, n_features, ratio_train_test, n_neighbors, chunk_size, strategy
+):
+    if n_samples < n_neighbors:
+        pytest.skip(
+            f"Skipping as n_samples (={n_samples}) < n_neighbors (={n_neighbors})",
+            allow_module_level=True,
+        )
+
+    np.random.seed(1)
+    X_train = np.random.rand(int(n_samples * n_features)).reshape((-1, n_features))
+    X_test = np.random.rand(int(n_samples * n_features / ratio_train_test)).reshape(
+        (-1, n_features)
+    )
+
+    knn_float64, _ = parallel_knn(
+        X_train.astype(np.float64),
+        X_test.astype(np.float64),
+        k=n_neighbors,
+        strategy=strategy,
+    )
+
+    knn_float32, _ = parallel_knn(
+        X_train.astype(np.float32),
+        X_test.astype(np.float32),
+        k=n_neighbors,
+        strategy=strategy,
+    )
+
+    np.testing.assert_array_equal(knn_float64, knn_float32)
